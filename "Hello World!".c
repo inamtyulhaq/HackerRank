02e6a584cbc836e168266f7db0b7d1876b35f855
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2024-01-06 23:08:37 +0500

    Update "Hello World!".c

--- "Hello World!".c
+++ "Hello World!".c
@@ -1,11 +1,13 @@
+// Question:
 /*This challenge requires you to print Hello, World! on a single line, 
 and then print the already provided input string to stdout. 
 If you are not familiar with C, you may want to read about the printf() command.*/
 
+// Explanation:
 /*It was simple. I simply printed the Hello World statement
 and then printed the string with string format specifier and that's it.*/
-// Solution:
 
+// Solution:
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
