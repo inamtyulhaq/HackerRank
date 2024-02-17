9a74060b97a7bb09f77de02f98406efcc30d57cd
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2024-02-17 18:30:19 +0500

    Create Hello World.c

---
+++ C Language/Hello World.c
@@ -0,0 +1,24 @@
+// Question:
+/*This challenge requires you to print Hello, World! on a single line, 
+and then print the already provided input string to stdout. 
+If you are not familiar with C, you may want to read about the printf() command.*/
+
+// Explanation:
+/*It was simple. I simply printed the Hello World statement
+and then printed the string with string format specifier and that's it.*/
+
+// Solution:
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+	
+    char s[100];
+    scanf("%[^\n]%*c", &s);
+  	printf("Hello, World!\n");
+    printf("%s", s);     
+    return 0;
+}
