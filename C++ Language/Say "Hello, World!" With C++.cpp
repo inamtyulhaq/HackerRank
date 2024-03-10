0c557b270726a1623a6d448984ee78ebb952734e
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2024-03-10 17:35:16 +0500

    Create Say "Hello, World!" With C++.cpp

---
+++ C++ Language/Say "Hello, World!" With C++.cpp
@@ -0,0 +1,15 @@
+// Question
+// https://www.hackerrank.com/challenges/cpp-hello-world/problem?isFullScreen=true
+
+// Explanation:
+// simply cout hello world
+
+// Solution:
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!";
+    return 0;
+}
