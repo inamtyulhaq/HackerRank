6aa7c46c6a3df1afbdf16988c6a1bb22ad55ded8
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2024-03-10 17:37:15 +0500

    Create Input and Output.cpp

---
+++ C++ Language/Input and Output.cpp
@@ -0,0 +1,22 @@
+// Question
+// https://www.hackerrank.com/challenges/cpp-input-and-output/problem?isFullScreen=true
+
+// Explanation:
+// simply input three numbers and output their sum
+
+// Solution:
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c; 
+    cout << a+b+c << endl;
+    return 0;
+}
+
